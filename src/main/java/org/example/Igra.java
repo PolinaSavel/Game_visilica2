23eca87f8842e5efd8083f4be92556b7d90acc90
PolinaSavel <108282505+PolinaSavel@users.noreply.github.com>
2024-10-04 14:49:13 +0500

    Update Igra.java
    new method

--- src/main/java/org/example/Igra.java
+++ src/main/java/org/example/Igra.java
@@ -12,10 +12,6 @@ public class Igra extends Question {
         this.bukva = bukva;
     }
 
-    public void setVerniyOtvet(boolean[] verniyOtvet) {
-        this.verniyOtvet = verniyOtvet;
-    }
-
     public void slovoPoBukvam(String topic) {
         String[] bukva = new String[topic.length()];                //zapisivaem v massiv bukvi slova
 
@@ -28,50 +24,55 @@ public class Igra extends Question {
     public void ugadivaemBukvu(String topic, Risunok risunok, String[] bukva) {
         boolean[] verniyOtvet = new boolean[topic.length()];
 
-        for (int i = 0; i < 11; i++) {          // 11 popitok 4tobi narisovat zayca
-            int peremennaya = 0;
-
-            for (int f = 0; f < topic.length(); f++) {
-                if (verniyOtvet[f]) {
-                    peremennaya = peremennaya + 1;
-                }
-            }
+        var kolvoPopitok = 11;
+        for (int i = 0; i < kolvoPopitok; i++) {          // 11 popitok 4tobi narisovat zayca
+            var peremennaya = initializePeremennaya(topic, verniyOtvet);
 
             if (peremennaya == topic.length()) {                      //esli slovo ugadano
                 risunok.setProigrish(true);
                 break;
-            } else {
-                System.out.println("\nVvedite vashu bukvu: ");
-                varianBukviIgroka = scanner.nextLine();
-                boolean neUgadalBukvu = false;
-                boolean ugadalBukvu = false;
+            }
+            System.out.println("\nVvedite vashu bukvu: ");
+            varianBukviIgroka = scanner.nextLine();
+            boolean neUgadalBukvu = false;
+            boolean ugadalBukvu = false;
 
-                for (int j = 0; j < topic.length(); j++) {
+            for (int j = 0; j < topic.length(); j++) {
 
-                    if (bukva[j].equals(varianBukviIgroka)) {                          //proverka po pervomu krugu
-                        System.out.println("Bukva " + bukva[j] + " pod nomerom: " + (j + 1));
-                        verniyOtvet[j] = true;      // esli bukvu ugadali, to true
-                        ugadalBukvu = true;
-                        neUgadalBukvu = false;
+                if (bukva[j].equals(varianBukviIgroka)) {                          //proverka po pervomu krugu
+                    System.out.println("Bukva " + bukva[j] + " pod nomerom: " + (j + 1));
+                    verniyOtvet[j] = true;      // esli bukvu ugadali, to true
+                    ugadalBukvu = true;
+                    neUgadalBukvu = false;
 
-                        for (int k = j + 1; k < topic.length(); k++) {    //vdrug v slove dve povtoryayushiesya bukvi
+                    for (int k = j + 1; k < topic.length(); k++) {    //vdrug v slove dve povtoryayushiesya bukvi
 
-                            if (bukva[k].equals(varianBukviIgroka)) {
-                                System.out.println("Eshe bukva " + bukva[k] + " pod nomerom: " + (k + 1));
-                                j = k + 1;
-                                verniyOtvet[k] = true;
-                            }
+                        if (bukva[k].equals(varianBukviIgroka)) {
+                            System.out.println("Eshe bukva " + bukva[k] + " pod nomerom: " + (k + 1));
+                            j = k + 1;
+                            verniyOtvet[k] = true;
                         }
-                    } else if (!ugadalBukvu) {                  //esli ne ugadali bukvu
-                        neUgadalBukvu = true;
                     }
+                } else if (!ugadalBukvu) {                  //esli ne ugadali bukvu
+                    neUgadalBukvu = true;
                 }
-                if (ugadalBukvu) {                       //vivodim otvet esli ugadali
-                    System.out.println("U vas ostalos " + (11 - (i + 1)) + " popitok");
-                } else if (neUgadalBukvu) {                                     //vivodim otvet esli ne ugadali
-                    System.out.println("Takoi bukvi net v slove, u vas ostalos " + (11 - (i + 1)) + " popitok");
-                }
+            }
+            if (ugadalBukvu) {                       //vivodim otvet esli ugadali
+                System.out.println("U vas ostalos " + (kolvoPopitok - (i + 1)) + " popitok");
+            } else if (neUgadalBukvu) {                                     //vivodim otvet esli ne ugadali
+                System.out.println("Takoi bukvi net v slove, u vas ostalos " + (kolvoPopitok - (i + 1)) + " popitok");
             }
         }
     }
+
+    public int initializePeremennaya(String topic, boolean[] verniyOtvet) {
+        int peremennaya = 0;
+        for (int f = 0; f < topic.length(); f++) {
+            if (verniyOtvet[f]) {
+                peremennaya = peremennaya + 1;
+            }
+        }
+        return peremennaya;
+    }
 }
+
